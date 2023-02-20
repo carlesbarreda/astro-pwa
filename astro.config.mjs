3ad502ed8d9acb5259c0864ab19112f265065d20
Carles Barreda <cbm@carlesbarreda.cat>
2023-02-20 15:28:30 +0100

    Removed PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-import AstroPWA from "@vite-pwa/astro";
-
 const FLAG = {
   mode: import.meta.env.PROD ? "production" : "development",
   site: import.meta.env.SITE ?? "https://carlesbarreda.github.io",
@@ -19,10 +17,11 @@ FLAG.base += !FLAG.base?.endsWith("/") ? "/" : "";
 
 console.log("* FLAG");
 console.log(FLAG);
-console.log("* import,meta.env");
+console.log("* import.meta.env");
 console.log(import.meta.env);
 
 // https://astro.build/config
+/*
 export default defineConfig({
   site: FLAG.site,
   base: FLAG.base,
@@ -33,65 +32,10 @@ export default defineConfig({
       __SITE__: JSON.stringify(FLAG.site),
       __BASE__: JSON.stringify(FLAG.base),
     },
-  },
-  integrations: [
-    AstroPWA({
-      disable: false,
-      mode: FLAG.mode,
-      base: FLAG.base,
-      scope: FLAG.base,
-      outDir: "dist",
-      registerType: "prompt",
-      strategies: "generateSW",
-      includeAssets: ["favicon.svg"],
-      buildBase: FLAG.base,
-      manifest: {
-        id: `${FLAG.base}?astro-pwa`,
-        start_url: `${FLAG.base}?standalone=true`,
-        name: "astro-pwa",
-        short_name: "astro-pwa",
-        description: "@vite-pwa/astro: PWA integration for Astro.",
-        background_color: "#ffffff",
-        theme_color: "#ffffff",
-        display: "standalone",
-        orientation: "natural",
-        dir: "ltr",
-        icons: [
-          {
-            src: `${FLAG.base}pwa/manifest-icon-192.maskable.png`,
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: `${FLAG.base}pwa/manifest-icon-192.maskable.png`,
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: `${FLAG.base}pwa/manifest-icon-512.maskable.png`,
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: `${FLAG.base}pwa/manifest-icon-512.maskable.png`,
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-      workbox: {
-        globDirectory: "dist",
-        globPatterns: [
-          "**/*.{js,html,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,txt}",
-        ],
-      },
-      devOptions: {
-        enabled: import.meta.env.DEV,
-      },
-    }),
-  ],
+  }
+});
+*/
+export default defineConfig({
+  site: FLAG.site,
+  base: FLAG.base
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,4 @@
 /// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/client" />
-/// <reference types="vite-plugin-pwa/info" />
-/// <reference types="vite-plugin-pwa" />
 
 declare const __SITE__: string;
 declare const __BASE__: string;
