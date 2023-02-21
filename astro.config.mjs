c7b1399bac762cc3a76bd5ed0c8e6d72a9b9986c
Carles Barreda <cbm@carlesbarreda.cat>
2023-02-21 22:50:26 +0100

    Update debug message

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import AstroPWA from "@vite-pwa/astro";
 
-const FLAG = {
-  mode: import.meta.env.PROD ? "production" : "development",
-  site: import.meta.env.SITE ?? "https://carlesbarreda.github.io",
-  base: import.meta.env.BASE_URL ?? "/astro-pwa",
-};
-
-// Parse Argo CLI flags
-for (let i = 0; i < process.argv.length; i++) {
-  if (process.argv[i] === "--site") FLAG.site = process.argv[++i];
-  if (process.argv[i] === "--base") FLAG.base = process.argv[++i];
-}
-
-// Add trailing slash if nedded
-FLAG.base += !FLAG.base?.endsWith("/") ? "/" : "";
-
-console.log("* FLAG");
-console.log(FLAG);
-console.log("* import.meta.env");
-console.log(import.meta.env);
+import { FLAG } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const FLAG = {
+  mode: import.meta.env.PROD ? "production" : "development",
+  site: import.meta.env.SITE ?? "https://carlesbarreda.github.io",
+  base: import.meta.env.BASE_URL ?? "/astro-pwa",
+};
+
+// Parse Argo CLI flags
+for (let i = 0; i < process.argv.length; i++) {
+  if (process.argv[i] === "--site") FLAG.site = process.argv[++i];
+  if (process.argv[i] === "--base") FLAG.base = process.argv[++i];
+}
+
+// Add trailing slash if nedded
+FLAG.base += !FLAG.base?.endsWith("/") ? "/" : "";
+
+console.log("* FLAG");
+console.log(FLAG);
+console.log("* import,meta.env");
+console.log(import.meta.env);
